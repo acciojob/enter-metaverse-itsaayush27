7fbbb8ead3a5ae203e25f30012f8011eeaa1420a
Aayush Gour <138443486+itsaayush27@users.noreply.github.com>
2025-09-01 14:33:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const status = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+   enterBtn.addEventListener("click", () => {
+      // Change paragraph content into an <h1> with new text
+      status.outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+    });
